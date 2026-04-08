1b2934e6ace5d4619130eb1a61c144859e5886c6
Arthur Gonçalves de Oliveira <oliveraarthurrtx@gmail.com>
2026-04-08 14:34:49 -0300

    finish my second basic project

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo");
+        System.out.println("Filme: Top gun Maverick");
+
+        int anoDeLancamento  = 2022;
+        System.out.println ("Ano de lacamento: " + anoDeLancamento);
+
+        String descricaoTopGunMaverick =("O filme conta a historia do piloto Maverick junto dos seus companheiros " +
+                "de equipe, e o filho do seu antigo companheiro de equipe Goosse");
+        System.out.println(descricaoTopGunMaverick);
+        boolean incluidoNoPlano = false;
+        double notaFilme = 9.1;
+        double notaDoFilmeCritica = 8.0;
+        System.out.println("Nota do Filme pelo publico: "+notaFilme);
+        System.out.println("Nota do Filme pela critica: "+notaDoFilmeCritica);
+    }
+}
