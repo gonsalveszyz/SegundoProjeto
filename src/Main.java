4de1ccd7845d47bf080a361068bc01b12d201667
Arthur Gonçalves de Oliveira <oliveraarthurrtx@gmail.com>
2026-04-09 13:29:47 -0300

    correction of the arithmetic mean

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,8 @@ public class Main {
         double notaDoFilmeCritica = 8.0;
         System.out.println("Nota do Filme pelo publico: "+notaFilme);
         System.out.println("Nota do Filme pela critica: "+notaDoFilmeCritica);
-        double media = 9.1 + 8.0 /2;
+        double media = (9.1 + 8.0) /2;
         System.out.println(media);
+
     }
 }
