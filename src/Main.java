1275a3dfe5ce14593f6c98406b0aa84cb413c345
Arthur Gonçalves de Oliveira <oliveraarthurrtx@gmail.com>
2026-04-09 13:42:24 -0300

    correction of the arithmetic mean and description corretion

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bem vindo");
         System.out.println("Filme: Top gun Maverick");
-
+        //bem vindo e nome do filme
         int anoDeLancamento  = 2022;
         System.out.println ("Ano de lacamento: " + anoDeLancamento);
-
+        //ano de lancamento
         String descricaoTopGunMaverick =("O filme conta a historia do piloto Maverick junto dos seus companheiros " +
                 "de equipe, e o filho do seu antigo companheiro de equipe Goosse");
         System.out.println(descricaoTopGunMaverick);
+        //descricao do filme
         boolean incluidoNoPlano = false;
         double notaFilme = 9.1;
         double notaDoFilmeCritica = 8.0;
         System.out.println("Nota do Filme pelo publico: "+notaFilme);
         System.out.println("Nota do Filme pela critica: "+notaDoFilmeCritica);
         double media = (9.1 + 8.0) /2;
+        //media do artimetica do filme
         String  mediaDosPublicos = (" A media aritimetica do filme foi de: " + media);
         System.out.println(mediaDosPublicos + " o filme teve uma boa avaliacao!");
+        //meritos finais
 
     }
 }
